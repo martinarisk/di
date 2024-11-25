412d7ac1572c2506e6131a44dd5703a2f097bc3d
martinarisk <148961996+martinarisk@users.noreply.github.com>
2024-11-25 14:54:51 +0100

    Update dependency_injection.go
    break recursion here

--- dependency_injection/dependency_injection.go
+++ dependency_injection/dependency_injection.go
@@ -106,7 +106,7 @@ func Any[T any](di *DependencyInjection, res *T) error {
 			return nil
 		}
 	}
-	if Any[DependencyInjection](di, di) == nil {
+	if di != res && Any[DependencyInjection](di, di) == nil {
 		if di != nil {
 			return Any[T](di, res)
 		}
